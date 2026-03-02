f00a7cb5d0199d8866c9ab51c121cbd14a607b99
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-02 21:08:07 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import MilkTea from "./components/01.Pure-Component/MilkTea";
+import TeaSet from "./components/02.Not-Pure-Component/TeaSet";
 
 const App = () => {
   return (
     <div>
-      <MilkTea />
+      <TeaSet />
     </div>
   );
 };
--- src/components/02.Not-Pure-Component/TeaSet.jsx
+++ src/components/02.Not-Pure-Component/TeaSet.jsx
@@ -1,17 +1,16 @@
 let guest = 0;
-const Cup = () => {
+
+const Cup = ({ guest }) => {
   guest = guest + 1;
   return <div>Tea cup for guest #{guest}</div>;
 };
 
 const TeaSet = () => {
-  return (
-    <>
-      <Cup /> 1
-      <Cup />
-      <Cup />
-    </>
-  );
+  const cups = [];
+  for (let i = 0; i <= 12; i++) {
+    cups.push(<Cup guest={i} key={i} />);
+  }
+  return <>{cups}</>;
 };
 
 export default TeaSet;
