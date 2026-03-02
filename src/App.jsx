521dfac3d63f9653b3be934b1a68b8b7faf40ee1
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-02 21:15:11 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import MilkTea from "./components/01.Pure-Component/MilkTea";
 import TeaSet from "./components/02.Not-Pure-Component/TeaSet";
+import Counter from "./components/03.Counter/Counter";
 
 const App = () => {
   return (
     <div>
-      <TeaSet />
+      <Counter />
     </div>
   );
 };
--- src/components/03.Counter/Counter.jsx
+++ src/components/03.Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const Counter = () => {
   const [number, setNumber] = useState(0);
@@ -7,12 +7,21 @@ const Counter = () => {
       <h1>{number}</h1>
       <button
         onClick={() => {
-          setNumber(number + 1);
-          setNumber(number + 1);
-          setNumber(number + 1);
+          setNumber(number + 5);
+          setNumber((n) => n + 1);
         }}>
         +3
       </button>
+
+      {/* <h1>{number}</h1>
+      <button
+        onClick={() => {
+          setNumber((n) => n + 1);
+          setNumber((n) => n + 1);
+          setNumber((n) => n + 1);
+        }}>
+        +3
+      </button> */}
     </div>
   );
 };
